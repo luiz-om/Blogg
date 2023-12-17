4ffe330a7c0eb8cf685816cf3274dbe5a7bc22eb
“Luiz-om” <luizfernando_martins@hotmail.com>
2023-12-17 18:45:05 -0400

    Inicializacao projeto

---
+++ Migrations/20231217223922_bancoCerto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blogg.Migrations
+{
+    /// <inheritdoc />
+    public partial class bancoCerto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
